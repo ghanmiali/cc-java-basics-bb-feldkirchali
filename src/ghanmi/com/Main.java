91337d0532dd5a3ed246885da679123920624bdf
Ali Ghanmi <lenssolution.ag@gmail.com>
2020-10-20 21:44:33 +0200

    ok

--- src/ghanmi/com/Main.java
+++ src/ghanmi/com/Main.java
@@ -3,14 +3,15 @@ package ghanmi.com;
 public class Main {
 
     public static void main(String[] args) {
-            First.textAnalyse();
+        First.textAnalyse();
 
-            RondomMethode.zufall();
-            RandomMethoden2.zufall2();
-        System.out.println(MethodenWärhungsrechner2.Exchange(10,1.07));
-        }
+        RondomMethode.zufall();
+        RandomMethoden2.zufall2();
+        System.out.println(MethodenWärhungsrechner2.Exchange(10, 1.07));
+        RandomSumme.zufallSumme();
+    }
 
-        }
+}
 
 
 
